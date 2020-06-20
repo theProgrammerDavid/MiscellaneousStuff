26cd4dc06b28900419196b254f2689d67c0621bf
theProgrammerDavid <davidvelho2awesome@gmail.com>
2020-06-20 23:58:29 +0530

    [WIP] CTF->Shell Coding

---
+++ ctf challenges/shell-coding/shellCoding.c
@@ -0,0 +1,14 @@
+
+void do_something() {
+    //get inputs
+    printf("Did something.");
+}
+
+void do_something_else() {
+    printf("Did something else.");
+}
+
+int main() {
+    do_something()
+    return 0;
+}
