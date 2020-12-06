30c57c3f7c8cb96f9179cab574dae5b1530318f1
theProgrammerDavid <davidvelho2awesome@gmail.com>
2020-12-06 23:11:19 +0530

    django: poll app

--- djangoProjects/pollster/polls/views.py
+++ djangoProjects/pollster/polls/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+from .models import Question, Choice
+
+# get questions and display them
+
+def index(request):
+    render(request, './templates/polls/index.html')
